41cf3d1879aae5b5432973617d3c66e2203d3b79
Ktlyctrk <ktlyyctrk@gmail.com>
2025-10-23 23:32:50 +0300

    Frmİnfo fixed

--- HorseRace/FrmInfo.Designer.cs
+++ HorseRace/FrmInfo.Designer.cs
@@ -28,35 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.DgvResult = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.DgvResult)).BeginInit();
+            this.DgvRaceDayResult = new System.Windows.Forms.DataGridView();
+            this.DgvRaceCardResult = new System.Windows.Forms.DataGridView();
+            this.DgvRaceResult = new System.Windows.Forms.DataGridView();
+            this.DgvHorseResult = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvRaceDayResult)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvRaceCardResult)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvRaceResult)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvHorseResult)).BeginInit();
             this.SuspendLayout();
             // 
-            // DgvResult
+            // DgvRaceDayResult
             // 
-            this.DgvResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DgvResult.Location = new System.Drawing.Point(117, 12);
-            this.DgvResult.Name = "DgvResult";
-            this.DgvResult.RowHeadersWidth = 51;
-            this.DgvResult.RowTemplate.Height = 24;
-            this.DgvResult.Size = new System.Drawing.Size(671, 426);
-            this.DgvResult.TabIndex = 0;
+            this.DgvRaceDayResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvRaceDayResult.Location = new System.Drawing.Point(12, 12);
+            this.DgvRaceDayResult.Name = "DgvRaceDayResult";
+            this.DgvRaceDayResult.RowHeadersWidth = 51;
+            this.DgvRaceDayResult.RowTemplate.Height = 24;
+            this.DgvRaceDayResult.Size = new System.Drawing.Size(420, 945);
+            this.DgvRaceDayResult.TabIndex = 0;
+            this.DgvRaceDayResult.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgvRaceDayResult_CellClick);
+            this.DgvRaceDayResult.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgvRaceDayResult_CellContentClick);
+            // 
+            // DgvRaceCardResult
+            // 
+            this.DgvRaceCardResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvRaceCardResult.Location = new System.Drawing.Point(467, 12);
+            this.DgvRaceCardResult.Name = "DgvRaceCardResult";
+            this.DgvRaceCardResult.RowHeadersWidth = 51;
+            this.DgvRaceCardResult.RowTemplate.Height = 24;
+            this.DgvRaceCardResult.Size = new System.Drawing.Size(420, 945);
+            this.DgvRaceCardResult.TabIndex = 0;
+            this.DgvRaceCardResult.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgvRaceCardResult_CellClick);
+            // 
+            // DgvRaceResult
+            // 
+            this.DgvRaceResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvRaceResult.Location = new System.Drawing.Point(925, 12);
+            this.DgvRaceResult.Name = "DgvRaceResult";
+            this.DgvRaceResult.RowHeadersWidth = 51;
+            this.DgvRaceResult.RowTemplate.Height = 24;
+            this.DgvRaceResult.Size = new System.Drawing.Size(420, 945);
+            this.DgvRaceResult.TabIndex = 0;
+            this.DgvRaceResult.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgvRaceResult_CellClick);
+            // 
+            // DgvHorseResult
+            // 
+            this.DgvHorseResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvHorseResult.Location = new System.Drawing.Point(1385, 12);
+            this.DgvHorseResult.Name = "DgvHorseResult";
+            this.DgvHorseResult.RowHeadersWidth = 51;
+            this.DgvHorseResult.RowTemplate.Height = 24;
+            this.DgvHorseResult.Size = new System.Drawing.Size(420, 945);
+            this.DgvHorseResult.TabIndex = 0;
+            
             // 
             // FrmInfo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.DgvResult);
+            this.ClientSize = new System.Drawing.Size(1817, 969);
+            this.Controls.Add(this.DgvHorseResult);
+            this.Controls.Add(this.DgvRaceResult);
+            this.Controls.Add(this.DgvRaceCardResult);
+            this.Controls.Add(this.DgvRaceDayResult);
             this.Name = "FrmInfo";
             this.Text = "FrmInfo";
-            ((System.ComponentModel.ISupportInitialize)(this.DgvResult)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvRaceDayResult)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvRaceCardResult)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvRaceResult)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvHorseResult)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView DgvResult;
+        private System.Windows.Forms.DataGridView DgvRaceDayResult;
+        private System.Windows.Forms.DataGridView DgvRaceCardResult;
+        private System.Windows.Forms.DataGridView DgvRaceResult;
+        private System.Windows.Forms.DataGridView DgvHorseResult;
     }
 }
--- HorseRace/FrmInfo.cs
+++ HorseRace/FrmInfo.cs
@@ -1,10 +1,12 @@
 ﻿using HorseRace.Models.ContextClasses;
+using HorseRace.Models.Entity;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,10 +15,73 @@ namespace HorseRace
 {
     public partial class FrmInfo : FrmBase
     {
+        MyContext _db;
         public FrmInfo()
         {
             InitializeComponent();
+            _db = new MyContext();
+            DgvRaceDayResult.DataSource = _db.RaceDays.ToList();
+            DgvRaceDayResult.CellClick += DgvRaceDayResult_CellClick;
+            DgvRaceCardResult.CellClick -= DgvRaceCardResult_CellClick;
         }
-        
+
+        private void DgvRaceDayResult_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void DgvRaceDayResult_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0)
+            {
+                return;
+            }
+            else
+            {
+                RaceDay raceDay = DgvRaceDayResult.Rows[e.RowIndex].DataBoundItem as RaceDay;
+                if (raceDay != null)
+                {
+                    DgvRaceCardResult.DataSource = raceDay.RaceCards;
+                }
+
+            }
+
+
+        }
+
+        private void DgvRaceCardResult_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0)
+            {
+                return;
+            }
+            else
+            {
+                RaceCard racecard = DgvRaceCardResult.Rows[e.RowIndex].DataBoundItem as RaceCard;
+                if (racecard != null)
+                {
+                    DgvRaceResult.DataSource = racecard.Races;
+                }
+            }
+        }
+
+        private void DgvRaceResult_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0)
+            {
+                return;
+            }
+            else
+            {
+                Race race = DgvRaceResult.Rows[e.RowIndex].DataBoundItem as Race;
+                if (race != null)
+                {
+                    DgvHorseResult.DataSource = race.Horses;
+                }
+            }
+        }
+
+
     }
 }
+
